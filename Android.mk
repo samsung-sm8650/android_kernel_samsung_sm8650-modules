2c0be1176f67b8d7eb88cb77a69d25dc5f0be0f4
Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>
2024-03-12 01:03:37 +0530

    asoc: Add compilation support for Volcano
    Add compilation support for volcano, includes
    wcd9378 driver compilation, and add dedicated
    configs for volcano which has different swr
    version.
    
    Change-Id: I8dffbe72ed524fcaaad8c9e65c7c993ad9678ae1
    Signed-off-by: Sarath Varma Ganapathiraju <quic_ganavarm@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -414,6 +414,24 @@ LOCAL_MODULE_TAGS         := optional
 LOCAL_MODULE_DEBUG_ENABLE := true
 LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
+###########################################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES           := $(AUDIO_SRC_FILES)
+LOCAL_MODULE              := wcd9378_dlkm.ko
+LOCAL_MODULE_KBUILD_NAME  := asoc/codecs/wcd9378/wcd9378_dlkm.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
+###########################################################
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES           := $(AUDIO_SRC_FILES)
+LOCAL_MODULE              := wcd9378_slave_dlkm.ko
+LOCAL_MODULE_KBUILD_NAME  := asoc/codecs/wcd9378/wcd9378_slave_dlkm.ko
+LOCAL_MODULE_TAGS         := optional
+LOCAL_MODULE_DEBUG_ENABLE := true
+LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
+include $(DLKM_DIR)/Build_external_kernelmodule.mk
 endif
 ifeq ($(call is-board-platform-in-list, pitti),true)
 ###########################################################
