qualcomm-linux/kgsl

kgsl: gen8: Fix GMU register capture in snapshot flow
When capturing GMU GX registers we need to ensure GX is ON,
while the rest of the GMU registers can be captured when GX is
OFF. Fix the GMU register capture so that we do not touch the
GX domain registers when GX might be OFF in snapshot flow.

Change-Id: Id8f389b01e8bbc11c148c0fad703cbbfb774535d
Signed-off-by: Urvashi Agrawal <quic_urvaagra@quicinc.com>

adreno_gen8_snapshot.c
@@ -28,8 +28,9 @@ const struct gen8_snapshot_block_list gen8_0_0_snapshot_block_list = {
 	.cx_debugbus_blocks_len = ARRAY_SIZE(gen8_cx_debugbus_blocks),
 	.external_core_regs = gen8_0_0_external_core_regs,
 	.num_external_core_regs = ARRAY_SIZE(gen8_0_0_external_core_regs),
-	.gmu_regs = gen8_gmu_registers,
-	.num_gmu_regs = ARRAY_SIZE(gen8_gmu_registers),
+	.gmu_cx_unsliced_regs = gen8_0_0_gmu_registers,
+	.gmu_gx_regs = gen8_gmu_gx_registers,
+	.num_gmu_gx_regs = ARRAY_SIZE(gen8_gmu_gx_registers),
 	.rscc_regs = gen8_0_0_rscc_rsc_registers,
 	.reg_list = gen8_0_0_reg_list,
 	.cx_misc_regs = gen8_0_0_cx_misc_registers,

samsung-sm8650/android_kernel_samsung_sm8650-modules

Signed-off-by: Kaushal Sanadhya <quic_ksanadhy@quicinc.com>

adreno_gen8_snapshot.c
@@ -28,8 +28,9 @@ const struct gen8_snapshot_block_list gen8_3_0_snapshot_block_list = {
 	.cx_debugbus_blocks_len = ARRAY_SIZE(gen8_cx_debugbus_blocks),
 	.external_core_regs = gen8_3_0_external_core_regs,
 	.num_external_core_regs = ARRAY_SIZE(gen8_3_0_external_core_regs),
-	.gmu_regs = gen8_3_0_gmu_regs,
-	.num_gmu_regs = ARRAY_SIZE(gen8_3_0_gmu_regs),
+	.gmu_cx_unsliced_regs = gen8_3_0_gmu_registers,
+	.gmu_gx_regs = gen8_3_0_gmu_gx_regs,
+	.num_gmu_gx_regs = ARRAY_SIZE(gen8_3_0_gmu_gx_regs),
 	.rscc_regs = gen8_3_0_rscc_rsc_registers,
 	.reg_list = gen8_3_0_reg_list,
 	.cx_misc_regs = gen8_3_0_cx_misc_registers,