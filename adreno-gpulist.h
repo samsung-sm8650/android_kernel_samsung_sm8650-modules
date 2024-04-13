f952aac5ed1e7074a278546b15c50ae26e4ba6db
Kaushal Sanadhya <quic_ksanadhy@quicinc.com>
2024-04-13 14:44:33 +0530

    kgsl: gen8: Enable preemption for Gen8_3_0 GPU
    Enable preemption for Gen8_3_0 GPU.
    
    Change-Id: I205ae7561779020d7a7ff7d3de7f02c73fbb2c31
    Signed-off-by: Kaushal Sanadhya <quic_ksanadhy@quicinc.com>

--- adreno-gpulist.h
+++ adreno-gpulist.h
@@ -2760,7 +2760,8 @@ static const struct adreno_gen8_core adreno_gpu_core_gen8_3_0 = {
 				  UINT_MAX, UINT_MAX, UINT_MAX, ANY_ID),
 		.compatible = "qcom,adreno-gpu-gen8-3-0",
 		.features = ADRENO_APRIV | ADRENO_IOCOHERENT |
-			ADRENO_CONTENT_PROTECTION | ADRENO_IFPC | ADRENO_BCL,
+			ADRENO_CONTENT_PROTECTION | ADRENO_IFPC | ADRENO_BCL |
+			ADRENO_PREEMPTION,
 		.gpudev = &adreno_gen8_hwsched_gpudev.base,
 		.perfcounters = &adreno_gen8_perfcounters,
 		.uche_gmem_alignment = SZ_64M,
