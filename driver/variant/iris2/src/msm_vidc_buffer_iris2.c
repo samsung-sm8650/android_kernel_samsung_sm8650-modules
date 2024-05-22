5e85abfbd12783da58269cc4124db19050b0d10d
Rajathi S <quic_rajathis@quicinc.com>
2024-05-22 18:50:07 +0530

    Video: driver: Fixing DPB count
      - If Splitmode is enabled DPB count should be first used
        as populated by FW else populated to default value of 4.
    
    Change-Id: I3fe5a65da6f7fdfe9882abd22f4b9a94437d9972
    Signed-off-by: Rajathi S <quic_rajathis@quicinc.com>

--- driver/variant/iris2/src/msm_vidc_buffer_iris2.c
+++ driver/variant/iris2/src/msm_vidc_buffer_iris2.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2023-2024 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include "hfi_property.h"
@@ -514,9 +514,10 @@ static int msm_buffer_dpb_count(struct msm_vidc_inst *inst)
 	/* decoder dpb buffer count */
 	if (is_decode_session(inst)) {
 		color_fmt = inst->capabilities[PIX_FMTS].value;
-		if (is_linear_colorformat(color_fmt))
-			count = inst->buffers.output.min_count;
-
+		if (is_linear_colorformat(color_fmt)) {
+			count = inst->fw_min_count ?
+				inst->fw_min_count : inst->buffers.output.min_count;
+		}
 		return count;
 	}
 
